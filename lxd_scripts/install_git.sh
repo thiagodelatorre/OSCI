d75cd9ab7daac0c5f50509feb267e0eb31987b4a
Thiago Delatorre <thiago.delatorre@gmail.com>
2017-07-03 06:49:41 -0400

    Finish nginx+git setup

--- lxd_scripts/install_git.sh
+++ lxd_scripts/install_git.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get -y install git git-core
 
-mkdir ~/git
-cd ~/git
+mkdir ~ubuntu/git
+cd ~ubuntu/git
 git init --bare repo.git
-cd ..
+cd -
--- lxd_scripts/install_nginx.sh
+++ lxd_scripts/install_nginx.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+# Thanks to:
+# https://superuser.com/questions/719949/centos-5-nginx-git-http-backend
+# 
+
 sudo apt-get -y install nginx git fcgiwrap apache2-utils
 
-sudo echo "fastcgi_param REMOTE_USER $remote_user;" >> /etc/nginx/fastcgi_params
+#sudo echo "fastcgi_param REMOTE_USER $remote_user;" >> /etc/nginx/fastcgi_params
 
 sudo ln -s /etc/nginx/sites-available/git /etc/nginx/sites-enabled/git
 
 sudo chmod -R g+ws /home/ubuntu/git
 sudo chgrp -R www-data /home/ubuntu/git
+mkdir /home/ubuntu/nginx
+chgrp www-data /home/ubuntu/nginx
+sudo chgrp www-data /var/run/fcgiwrap.socket
 
 sudo htpasswd -c /etc/nginx/passwd ubuntu
 
