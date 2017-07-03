e91e203f564d88c9cbc7c9e593c951960cf68693
Thiago Delatorre <thiago.delatorre@gmail.com>
2017-07-03 08:24:47 -0400

    Fix push of selenium test file

--- files.sh
+++ files.sh
@@ -3,3 +3,5 @@
 for i in lxd_scripts/*.sh ; do lxc file push $i OSCI/tmp/ ; done
 lxc exec OSCI -- mkdir -p /etc/nginx/sites-available/
 lxc file push nginx/git OSCI/etc/nginx/sites-available/
+
+lxc file push selenium/* OSCI/tmp/
