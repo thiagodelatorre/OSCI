38d15824977c1ff0eb0abe2c723567f12ff9ad5b
Thiago Delatorre <thiago.delatorre@gmail.com>
2017-07-03 07:03:37 -0400

    Fix files.sh

--- files.sh
+++ files.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-for i in *.sh ; do lxc file push $i OSCI/tmp/ ; done
+for i in lxd_scripts/*.sh ; do lxc file push $i OSCI/tmp/ ; done
 lxc exec OSCI -- mkdir -p /etc/nginx/sites-available/
 lxc file push nginx/git OSCI/etc/nginx/sites-available/
